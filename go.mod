30859e0f04cb87787dbf85224f72384c718087c1
Muhammad Bagas Pradana <bagaspradana0502@gmail.com>
2022-12-10 12:44:00 +0700

    update app v2

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/bagas050201/go-app-hello
 
 go 1.17
 
-require github.com/bagas050201/go-say-hello-web v1.0.0
+require github.com/gorilla/mux v1.8.0
+
+require github.com/bagas050201/go-say-hello-web/v2 v2.1.0
--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
-
 import (
-	"fmt"
-	go_say_hello_web "github.com/bagas050201/go-say-hello-web"
+    "net/http"
+    "log"
+    "github.com/gorilla/mux"
+    go_say_hello_web "github.com/bagas050201/go-say-hello-web/v2"
 )
 
+func Handler(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Hello World!\n"))
+    w.Write([]byte(go_say_hello_web.SayHello("Bagas","jl. test 10")))
+}
+
 func main() {
-	fmt.Println("t")
+    r := mux.NewRouter()
+    r.HandleFunc("/", Handler)
+    log.Fatal(http.ListenAndServe(":8000", r))
 }
