fd0bcb4a7d9ac8bb57f5da9972d328a73d8077d2
Muhammad Bagas Pradana <bagaspradana0502@gmail.com>
2022-12-10 11:17:50 +0700

    membuat module go-app-hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bagas050201/go-app-hello
+
+go 1.17
+
+require github.com/bagas050201/go-say-hello-web v1.0.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+
+import (
+	"fmt"
+	go_say_hello_web "github.com/bagas050201/go-say-hello-web"
+)
+
+func main() {
+	fmt.Println("t")
+}
